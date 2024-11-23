c60e6e2c3231cc3db342790cda1635a85933b18f
Shashank <shashankbmusale07@gmail.com>
2024-11-23 10:23:05 +0530

    upd

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+        <Header />
+        <div>
+        <img className="h-screen w-full" src="https://assets.nflxext.com/ffe/siteui/vlv3/4690cab8-243a-4552-baef-1fb415632f74/web/IN-en-20241118-TRIFECTA-perspective_0b813abc-8365-4a43-a9d8-14c06e84c9f3_small.jpg" alt="logo" />
+        </div>
+    </div>
+  )
+}
+
+export default Login;
