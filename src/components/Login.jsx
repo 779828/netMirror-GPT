ada1405579a444d0d2ee0eecccb928de5fcf07dc
Shashank <shashankbmusale07@gmail.com>
2024-11-23 14:14:04 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,76 @@
-import Header from './Header';
+import { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  const fullName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    
+    email.current.value = "";
+    password.current.value = "";
+    if (!isSignInForm && fullName.current) {
+      fullName.current.value = "";
+    }
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
-        <Header />
-        <div>
-        <img className="h-screen w-full" src="https://assets.nflxext.com/ffe/siteui/vlv3/4690cab8-243a-4552-baef-1fb415632f74/web/IN-en-20241118-TRIFECTA-perspective_0b813abc-8365-4a43-a9d8-14c06e84c9f3_small.jpg" alt="logo" />
-        </div>
+      <Header />
+      <div>
+        <img
+          className="absolute"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/4690cab8-243a-4552-baef-1fb415632f74/web/IN-en-20241118-TRIFECTA-perspective_0b813abc-8365-4a43-a9d8-14c06e84c9f3_small.jpg"
+          alt="logo"
+        />
+      </div>
+      <form
+        className=" w-1/4 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-xl bg-opacity-80"
+        
+      > 
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && <input
+          ref={fullName}
+          type="text"
+          placeholder="Full Name"
+          className="bg-gray-700 p-4 my-4 w-full"
+        />}
+        <input
+          ref={email}
+          type="email"
+          placeholder="Email Address"
+          className="bg-gray-700 p-4 my-4 w-full"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="bg-gray-700 p-4 my-4 w-full"
+        />
+        <p className="text-red-500">{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-800 rounded-lg w-full" onClick={handleButtonClick}>
+        {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="p-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now."} </p>
+      </form>
     </div>
   )
-}
+};
 
 export default Login;
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7F_i39bDGnoRCnuo2A257EM2PBqL-HEM",
+  authDomain: "netmirrorgpt.firebaseapp.com",
+  projectId: "netmirrorgpt",
+  storageBucket: "netmirrorgpt.firebasestorage.app",
+  messagingSenderId: "813242057047",
+  appId: "1:813242057047:web:2ffe522b02504e5ae23e12",
+  measurementId: "G-RPD2S6C5NM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
